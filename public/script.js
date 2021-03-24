bcec94cfa9377cffab8df8bba1b82e8e874d5c72
cfeld7 <cbfeld99@gmail.com>
2021-03-24 17:24:54 -0400

    dining

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 async function windowActions() {
-  const request = await fetch('/api/meals');
-  meals = await request.json();
+  const request = await fetch('/api/dining');
+  dining = await request.json();
 }
 window.onload = windowActions;
 console.log('window loaded');

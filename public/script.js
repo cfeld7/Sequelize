06007d324322dd634c2fc0cc569dca9c48026990
cfeld7 <cbfeld99@gmail.com>
2021-04-06 18:20:48 -0400

    Moved script.js to head and imported cdn script

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 async function windowActions() {
   const request = await fetch('/api/dining');
   dining = await request.json();
-  console.log(dining)
+  console.log(dining);
 }
 window.onload = windowActions;
 console.log('window loaded');

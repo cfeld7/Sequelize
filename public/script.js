a69f880f86c5484114ac1a8c041ba1d49a512341
cfeld7 <cbfeld99@gmail.com>
2021-04-08 16:43:29 -0400

    lab

--- public/script.js
+++ public/script.js
@@ -3,5 +3,26 @@ async function windowActions() {
   dining = await request.json();
   console.log(dining);
 }
+
+router.route('/wholeMeal')
+  .get(async(req, res) => {
+    const meals = await db.Meals.findall();
+    const macros = await db.Macros.findall();
+    const wholeMeals = meals.map((meal) => {
+      const macroEntry = macros.find((macro) => macro.meal_id === meal.meal_id);
+      console.log('meal', meal.dataValues);
+      return {
+        ...meal.dataValues,
+        ...macroEntry.dataValues
+      };
+    });
+  });
+
+async function getMeals() {
+  console.log('data request');
+  const diningRequest = await fetch('/api/wholeMeal');
+  const diningData = 
+}
+
 window.onload = windowActions;
 console.log('window loaded');
